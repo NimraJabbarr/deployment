bdf5032e107f7d4e3161f99845476b08b74d55cb
Nimra Jabbar <nimra.jabbarr@gmail.com>
2026-06-04 00:41:36 +0500

    feat: add runtime migrations to wsgi.py for Vercel deployment

--- school_crm/school_crm/wsgi.py
+++ school_crm/school_crm/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import django
+from django.core.management import call_command
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_crm.settings')
+
+django.setup()
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_crm.settings')
+# Run migrations on startup (only once per environment)
+if not os.environ.get('MIGRATIONS_RUN'):
+    try:
+        call_command('migrate', verbosity=0)
+        os.environ['MIGRATIONS_RUN'] = 'true'
+    except Exception as e:
+        print(f"Migration warning (non-fatal): {e}")
 
 application = get_wsgi_application()
